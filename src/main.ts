dc47a4b828c0605e179194a21fea5e8411b90f61
TheRoy-99 <theroymrodriguez@gmail.com>
2025-09-22 16:44:21 -0500

    Configuracoipon cors para frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Habilitar CORS antes de escuchar
+  app.enableCors({
+    origin: 'http://localhost:5173', // frontend Vite
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`🚀  Application is running on: http://localhost:${port}`);
